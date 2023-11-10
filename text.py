0ecc481a7b8290ad86f767623836b5d12fa2650c
Asger Schultz <asger.s@protonmail.com>
2023-11-10 18:45:47 +0100

    Wabbajack

--- text.py
+++ text.py
@@ -36,10 +36,10 @@ def api_fun(func) -> Callable:
 @api_fun
 def predict():
     data = _get_data()
-    # middle_boi = sorted(lens := [len(s.split()) for s in data])[len(data) // 2]
-    # preds = [1 if abe > middle_boi else 0 for abe in lens]
-    with open("cached_res.json", "r", encoding="utf-8") as file:
-        preds = json.load(file)
+    middle_boi = sorted(lens := [len(s.split()) for s in data])[len(data) // 2]
+    preds = [1 if abe > middle_boi else 0 for abe in lens]
+    #with open("cached_res.json", "r", encoding="utf-8") as file:
+    #    preds = json.load(file)
     for i, ex in enumerate(data):
         if "vigtigt at huske" in ex:
             preds[i] = 1
