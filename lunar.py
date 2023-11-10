096176efbe5d9752a835afe2ba2bc5d56153dc60
Asger Schultz <asger.s@protonmail.com>
2023-11-10 19:05:25 +0100

    Månelandingen blev faket

--- lunar.py
+++ lunar.py
@@ -4,12 +4,13 @@ import random
 from functools import wraps
 from typing import Callable
 
-import cv2
 import numpy as np
 from flask import Flask, request, jsonify
 from flask_restful import Api
 from flask_cors import CORS
 from pelutils import log
+
+from lunar_lander.run_agent import load_agent
 
 
 app = Flask(__name__)
@@ -19,7 +20,6 @@ CORS(app)
 def _get_data() -> tuple[list[float], float, bool, float, int]:
     """Returns data from a post request"""
     data = json.loads(request.data.decode("utf-8"))
-    print(data)
     return data["observation"], data["reward"], data["is_terminal"], data["total_reward"], data["game_ticks"]
 
 def api_fun(func) -> Callable:
@@ -36,11 +36,13 @@ def api_fun(func) -> Callable:
 @api_fun
 def predict():
     obs, reward, is_terminal, total_reward, game_ticks = _get_data()
-    return { "action": 0 }
+    action = agent.act(obs)
+    return { "action": action }
 
 if __name__ == "__main__":
     log.configure(
         "lunar.log",
         append=True,
     )
+    agent = load_agent("lunar_lander/agent_dqn_7.tar", True)
     app.run(host="0.0.0.0", port=6971, debug=False, processes=1, threaded=False)
