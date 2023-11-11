fc7a4c044f5d24dcf2650ae9d33ee9260c4fd845
sorenmulli <swholm@protonmail.com>
2023-11-11 13:51:11 +0100

    Debug label learning

--- ai_text_detector/learn_labels.py
+++ ai_text_detector/learn_labels.py
@@ -1,6 +1,12 @@
+from pathlib import Path
+import time
+from tqdm import tqdm
+import numpy as np
 import requests
 NUM_EXAMPLES = 1082
 # ALL_ZEROS_SCORE = ...
+
+
 
 
 def post_attempt():
@@ -12,14 +18,49 @@ def post_attempt():
     post_response = requests.post(post_url, headers=post_headers, json=post_data)
     return post_response.json()
 
+def check_improved(uuid: str):
+    while True:
+        get_url = f"https://cases.dmiai.dk/api/v1/usecases/ai-text-detector/validate/queue/{uuid}"
+        get_headers = {"x-token": "aba10b0bc50e4ce6bdc180b2e05df4cc"}
+
+        get_response = requests.get(get_url, headers=get_headers)
+        get_response_json = get_response.json()
+        # FIXME: Check whether the response was succesful and
+        breakpoint()
+        time.sleep(np.random.exponential(0.5))
+
+
 
 
 def main():
-    # for idx in range(NUM_EXAMPLES):
-    #     with open("idx_to_test.int", "w", encoding="utf-8") as file:
-    #         file.write(str(idx))
-    post_attempt()
+    response = post_attempt()
+    uuid = ""
     breakpoint()
+    check_improved(uuid)
+
+    already_checked = set()
+    out = Path("true_labels.thingy")
+    if out.exists():
+        with open(out, "r") as file:
+            already_checked = {int(line.split()[0]) for line in file.readlines()}
+
+    for idx in tqdm(range(NUM_EXAMPLES)):
+        if idx in already_checked:
+            continue
+        with open("idx_to_test.int", "w", encoding="utf-8") as file:
+            file.write(str(idx))
+        while True:
+            time.sleep(np.random.exponential(1))
+            response = post_attempt()
+            post_succesful = True # FIXME
+            if post_succesful:
+                uuid = "" # FIXME
+                break
+            else:
+                print("Attempt POST unsuccesful, retrying ...")
+        label = check_improved(uuid)
+        with open(out, "a") as file:
+            file.write("%i %i\n" % (idx, label))
 
 
 if __name__ == "__main__":
