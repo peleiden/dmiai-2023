c9292ec8440bc30e48ba05d84a0320b295bd4935
Asger Schultz <asger.s@protonmail.com>
2023-11-11 10:55:19 +0100

    Gem data

--- lunar.py
+++ lunar.py
@@ -8,7 +8,7 @@ import numpy as np
 from flask import Flask, request, jsonify
 from flask_restful import Api
 from flask_cors import CORS
-from pelutils import log
+from pelutils import log, TT
 
 from lunar_lander.run_agent import load_agent
 
@@ -32,17 +32,34 @@ def api_fun(func) -> Callable:
 
     return wrapper
 
+d = [list()]
+gameno = 0
 @app.route("/predict", methods=["POST"])
 @api_fun
 def predict():
+    global gameno
     obs, reward, is_terminal, total_reward, game_ticks = _get_data()
     action = agent.act(obs)
+    d[-1].append({
+        "obs": obs,
+        "reward": reward,
+        "total_reward": total_reward,
+        "game_ticks": game_ticks,
+        "action": action,
+    })
+    log.debug(f"{game_ticks:02} {action} {reward:2f} {total_reward:2f}")
+    if is_terminal:
+        with open("json_%i.json" % gameno, "w") as f:
+            import json
+            json.dump(d[-1], f, indent=4)
+        gameno += 1
+        d.append(list())
     return { "action": action }
 
 if __name__ == "__main__":
     log.configure(
         "lunar.log",
-        append=True,
+        print_level=0,
     )
-    agent = load_agent("lunar_lander/agent_dqn_7.tar", True)
+    agent = load_agent("lunar_lander/agent-dqn.pt", True)
     app.run(host="0.0.0.0", port=6971, debug=False, processes=1, threaded=False)
