b47a0f950203977c824acd423f5ed4bc129e2a32
sorenmulli <swholm@protonmail.com>
2023-11-11 13:26:02 +0100

    KEEP learnin

--- ai_text_detector/learn_labels.py
+++ ai_text_detector/learn_labels.py
@@ -1,11 +1,26 @@
-NUM_EXAMPLES = ...
-ALL_ZEROS_SCORE = ...
+import requests
+NUM_EXAMPLES = 1082
+# ALL_ZEROS_SCORE = ...
+
+
+def post_attempt():
+    post_url = "https://cases.dmiai.dk/api/v1/usecases/ai-text-detector/validate/queue"
+    post_headers = {"x-token": "aba10b0bc50e4ce6bdc180b2e05df4cc"}
+    post_data = {
+        "url": "http://82.211.207.131:6969/predict"
+    }
+    post_response = requests.post(post_url, headers=post_headers, json=post_data)
+    return post_response.json()
+
 
 
 def main():
-    for idx in range(NUM_EXAMPLES):
-        with open("idx_to_test.int", "w", encoding="utf-8") as file:
-            file.write(str(idx))
+    # for idx in range(NUM_EXAMPLES):
+    #     with open("idx_to_test.int", "w", encoding="utf-8") as file:
+    #         file.write(str(idx))
+    post_attempt()
+    breakpoint()
+
 
 if __name__ == "__main__":
     main()
