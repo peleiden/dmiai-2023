be7441852e4e3c25c65e99621976e6b582c2d222
sorenmulli <swholm@protonmail.com>
2023-11-15 23:27:47 +0100

    Add actual prediction

--- text.py
+++ text.py
@@ -1,14 +1,16 @@
+import os
 import json
 from functools import wraps
 from typing import Callable
+from glob import glob as glob # glob
 
 from flask import Flask, request, jsonify
 from flask_restful import Api
 from flask_cors import CORS
-from pelutils import log
+from pelutils import Parser, log
 import spacy
 
-# from ai_text_detector.exwerymemenwtation import sentence_complexity
+from ai_text_detector.training.eval import Classifier
 
 # python -m spacy download da_core_news_sm
 nlp = spacy.load("da_core_news_sm")
@@ -36,10 +38,13 @@ def api_fun(func) -> Callable:
 @api_fun
 def predict():
     data = _get_data()
-    preds = [0] * len(_get_data())
-    with open("idx_to_test.int", "r", encoding="utf-8") as file:
-        idx_to_test = int(file.read())
-    preds[idx_to_test] = 1
+    preds = MODEL.predict(data, BATCH_SIZE)
+    # FIXME FIXME FIXME REMOVE!
+    preds = [1 - p for p in preds]
+    # preds = [0] * len(_get_data())
+    # with open("idx_to_test.int", "r", encoding="utf-8") as file:
+    #     idx_to_test = int(file.read())
+    # preds[idx_to_test] = 1
     #middle_boi = sorted(lens := [len(s.split()) for s in data])[len(data) // 2]
     #preds = [1 if abe > middle_boi else 0 for abe in lens]
     ##with open("cached_res.json", "r", encoding="utf-8") as file:
@@ -53,6 +58,9 @@ def predict():
 
 
 if __name__ == "__main__":
+    location = Parser().parse_args().location
+    MODEL = Classifier(glob(os.path.join(location, "dfm-encoder-large-v1-idx*-ai-detector")))
+    BATCH_SIZE = 8
     log.configure(
         "text.log",
         append=True,
