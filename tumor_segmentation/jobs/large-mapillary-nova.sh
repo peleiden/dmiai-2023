ae47bb349b4ac178e8690f97e174c68373ca6f13
sorenmulli <swholm@protonmail.com>
2023-11-15 13:08:18 +0100

    Try ACTUALLY changing zhe model

---
+++ tumor_segmentation/jobs/large-mapillary-nova.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#BSUB -q gpua100
+#BSUB -gpu "num=1::mode=exclusive_process"
+##BSUB -R "select[gpu32gb]"
+
+#BSUB -n 4
+#BSUB -R "rusage[mem=20GB]"
+#BSUB -R "span[hosts=1]"
+
+#BSUB -W 8:00
+
+#BSUB -J "tumor"
+#BSUB -N
+#BSUB -u s183911@student.dtu.dk
+#BSUB -oo /zhome/ac/c/137651/joblogs/stdout_%J.out
+#BSUB -eo /zhome/ac/c/137651/joblogs/stderr_%J.out
+
+echo "Starting job on GPU $CUDA_VISIBLE_DEVICES ..."
+
+TPATH=/work3/s183911/toomah
+export HUGGINGFACE_HUB_CACHE=/zhome/ac/c/137651/.shitty_hf_cache
+source /zhome/ac/c/137651/dmiai-setup.sh
+export PYTHONPATH=$PYTHONPATH:.
+
+python tumor_segmentation/train.py\
+    $TPATH\
+    -c tumor_segmentation/jobs/large-mapillary-nova.ini
+
+
+
+echo "Finished job !"
