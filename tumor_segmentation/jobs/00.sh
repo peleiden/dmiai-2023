0ab89d4c5986483c0487daaec7c1c10306af5d32
asgerius <asger.s@protonmail.com>
2023-11-17 00:25:22 +0100

    du skal have mine jobs om du vil det eller ej

---
+++ tumor_segmentation/jobs/00.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#BSUB -q gpuv100
+#BSUB -J 00
+#BSUB -R select[gpu32gb]
+#BSUB -gpu num=1:mode=exclusive_process
+#BSUB -R rusage[mem=15GB]
+#BSUB -n 4
+#BSUB -W 24:00
+#BSUB -o $HOME/joblogs/%J.out
+#BSUB -e $HOME/joblogs/%J.err
+#BSUB -u s183912@dtu.dk
+#BSUB -N
+
+source ~/.hpcrc
+
+python3 tumor_segmentation/train.py /work3/s183912/tooomaaah -c tumor_segmentation/jobs/00.ini
